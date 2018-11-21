512cc2c456fdfcc7c1aed33c7a888fdec7a4f7b1
Andrew Lock <andrewlock.net@gmail.com>
2018-11-21 20:51:48 +0000

    Add CustomTwilioRestClient Demo

---
+++ src/CustomTwilioRestClientDemo/CustomTwilioClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Twilio.Clients;
+using Twilio.Http;
+
+namespace CustomTwilioRestClientDemo
+{
+    public class CustomTwilioClient : ITwilioRestClient
+    {
+        private readonly ITwilioRestClient _innerClient;
+
+        public CustomTwilioClient(IConfiguration config, System.Net.Http.HttpClient httpClient)
+        {
+            // customise the underlying HttpClient
+            httpClient.DefaultRequestHeaders.Add("X-Custom-Header", "HttpClientFactory-Sample");
+
+            _innerClient = new TwilioRestClient(
+                config["Twilio:AccountSid"],
+                config["Twilio:AuthToken"],
+                httpClient: new SystemNetHttpClient(httpClient));
+        }
+
+        public Response Request(Request request) => _innerClient.Request(request);
+        public Task<Response> RequestAsync(Request request) => _innerClient.RequestAsync(request);
+        public string AccountSid => _innerClient.AccountSid;
+        public string Region => _innerClient.Region;
+        public Twilio.Http.HttpClient HttpClient => _innerClient.HttpClient;
+    }
+}
---
+++ src/CustomTwilioRestClientDemo/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace CustomTwilioRestClientDemo
+{
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly ITwilioRestClient _client;
+
+        public MessageController(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost("api/send-sms")]
+        public IActionResult SendSms(MessageModel model)
+        {
+            var message = MessageResource.Create(
+                to: new PhoneNumber(model.To),
+                from: new PhoneNumber(model.From),
+                body: model.Message,
+                client: _client);
+
+            return Ok(message.Sid);
+        }
+
+        public class MessageModel
+        {
+            public string To { get; set; }
+            public string From { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
