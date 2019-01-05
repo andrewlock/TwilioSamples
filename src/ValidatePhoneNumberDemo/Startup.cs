8a930735c972d41054f8e574960ac14cf7386e65
Andrew Lock <andrewlock.net@gmail.com>
2019-01-05 13:52:34 +0000

    Install Twilio Client and initialise

--- src/ValidatePhoneNumberDemo/Startup.cs
+++ src/ValidatePhoneNumberDemo/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using ValidatePhoneNumberDemo.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Twilio;
 
 namespace ValidatePhoneNumberDemo
 {
@@ -44,6 +45,10 @@ namespace ValidatePhoneNumberDemo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var accountSid = Configuration["Twilio:AccountSID"];
+            var authToken = Configuration["Twilio:AuthToken"];
+            TwilioClient.Init(accountSid, authToken);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
