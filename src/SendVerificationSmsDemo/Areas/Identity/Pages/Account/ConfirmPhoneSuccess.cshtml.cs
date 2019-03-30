d922a6f9831bd26bd8d54ca6264fe353937efd4b
Andrew Lock <andrewlock.net@gmail.com>
2019-03-30 14:44:07 +0000

    Add VerifyPhone, ConfirmPhone, and ConfirmPhoneSuccess pages

---
+++ src/SendVerificationSmsDemo/Areas/Identity/Pages/Account/ConfirmPhoneSuccess.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SendVerificationSmsDemo.Areas.Identity.Pages.Account
+{
+    public class ConfirmPhoneSuccessModel : PageModel
+    {
+    }
+}
