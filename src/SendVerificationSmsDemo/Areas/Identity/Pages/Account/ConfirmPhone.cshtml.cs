73809431e8b769183d32690736a291634ace4463
Andrew Lock <andrewlock.net@gmail.com>
2019-04-11 22:56:58 +0100

    Add ConfirmPhone Razor page

---
+++ src/SendVerificationSmsDemo/Areas/Identity/Pages/Account/ConfirmPhone.cshtml.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SendVerificationSmsDemo.Areas.Identity.Pages.Account
+{
+    [Authorize]
+    public class ConfirmPhoneModel : PageModel
+    {
+        private readonly TwilioVerifyClient _client;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ConfirmPhoneModel(TwilioVerifyClient client, UserManager<IdentityUser> userManager)
+        {
+            _client = client;
+            _userManager = userManager;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Country dialing code")]
+            public int DialingCode { get; set; }
+
+            [Required]
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [Display(Name = "Code")]
+            public string VerificationCode { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                var result = await _client.CheckVerificationCode(Input.DialingCode, Input.PhoneNumber, Input.VerificationCode);
+                if (result.Success)
+                {
+                    var identityUser = await _userManager.GetUserAsync(User);
+                    identityUser.PhoneNumberConfirmed = true;
+                    var updateResult = await _userManager.UpdateAsync(identityUser);
+
+                    if (updateResult.Succeeded)
+                    {
+                        return RedirectToPage("ConfirmPhoneSuccess");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "There was an error confirming the verification code, please try again");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", $"There was an error confirming the verification code: {result.Message}");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("",
+                    "There was an error confirming the code, please check the verification code is correct and try again");
+            }
+
+            return Page();
+        }
+    }
+}
