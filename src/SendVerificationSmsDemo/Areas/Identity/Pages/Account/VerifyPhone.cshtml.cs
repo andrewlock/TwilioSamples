736c8216c5b4b41255d2fd93f31d75e5081c521a
Andrew Lock <andrewlock.net@gmail.com>
2019-04-11 22:56:10 +0100

    Add VerifyPhone Razor Page

---
+++ src/SendVerificationSmsDemo/Areas/Identity/Pages/Account/VerifyPhone.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SendVerificationSmsDemo.Areas.Identity.Pages.Account
+{
+    [Authorize]
+    public class VerifyPhoneModel : PageModel
+    {
+        private readonly TwilioVerifyClient _client;
+
+        public VerifyPhoneModel(TwilioVerifyClient client)
+        {
+            _client = client;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Country dialing code")]
+            public int DialingCode { get; set; }
+
+            [Required]
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                var result = await _client.StartVerification(Input.DialingCode, Input.PhoneNumber);
+                if (result.Success)
+                {
+                    return RedirectToPage("ConfirmPhone", new { Input.DialingCode, Input.PhoneNumber });
+                }
+
+                ModelState.AddModelError("", $"There was an error sending the verification code: {result.Message}");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("",
+                    "There was an error sending the verification code, please check the phone number is correct and try again");
+            }
+
+            return Page();
+        }
+    }
+}
